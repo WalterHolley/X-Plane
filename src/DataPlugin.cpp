3892f577c1c86b6783d1afbfa96b70680dc6cdf5
Zero <holley.walter@gmail.com>
2023-07-01 16:52:34 -0500

    Initial CMake build

--- src/DataPlugin.cpp
+++ src/DataPlugin.cpp
@@ -2,11 +2,29 @@
 // Created by Zero on 6/25/2023.
 //
 
+#define XPLM XPLM400
+
+#ifdef _WIN32
+#define IBM 1
+#endif
+
+#ifdef __MACH__
+#define APL apl
+#endif
+
+#ifdef __linux__
+#define LIN
+#endif
+
+
+
+
 #include <stdio.h>
 #include <string.h>
-#include "SDK/CHeaders/XPLM/XPLMDataAccess.h"
-#include "SDK/CHeaders/XPLM/XPLMPlugin.h"
-#include "SDK/CHeaders/XPLM/XPLMProcessing.h"
+#include "../SDK/CHeaders/XPLM/XPLMDataAccess.h"
+#include "../SDK/CHeaders/XPLM/XPLMPlugin.h"
+#include "../SDK/CHeaders/XPLM/XPLMProcessing.h"
+
 
 //Planned use APIs
 //https://developer.x-plane.com/sdk/XPLMDataAccess/
@@ -20,12 +38,12 @@ PLUGIN_API int XPluginStart(char * name, char * sig, char * desc)
     //basic plugin information
     strcpy(name, "BeigeBox Recorder");
     strcpy(sig, "com.avidata.recorder");
-    strcpy("Sim Flight Data Recorder for varied data");
+    strcpy(desc, "Sim Flight Data Recorder for varied data");
 
     //start network connection
 
     //register callback
-
+    return 1;
 }
 
 PLUGIN_API void XPluginStop(void)
