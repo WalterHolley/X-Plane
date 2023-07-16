423c9fbb072f8f4c976de988a75ad1522a2f0fa1
Zero <holley.walter@gmail.com>
2023-07-16 18:00:21 -0500

    Plugin Updates -Successful loading in windows 64 bit environment -Test for data refs successful -writing datarefs to file.

--- src/DataPlugin.cpp
+++ src/DataPlugin.cpp
@@ -1,7 +1,7 @@
 //
 // Created by Zero on 6/25/2023.
 //
-
+/*
 #define XPLM XPLM400
 
 #ifdef _WIN32
@@ -15,34 +15,77 @@
 #ifdef __linux__
 #define LIN
 #endif
-
+*/
 
 
 
 #include <stdio.h>
 #include <string.h>
+#include <ctime>
+#include <chrono>
+
 #include "../SDK/CHeaders/XPLM/XPLMDataAccess.h"
-#include "../SDK/CHeaders/XPLM/XPLMPlugin.h"
-#include "../SDK/CHeaders/XPLM/XPLMProcessing.h"
+#include "../SDK/CHeaders/XPLM/XPLMUtilities.h"
+#include "../SDK//CHeaders//XPLM//XPLMProcessing.h"
+
+
 
 
 //Planned use APIs
 //https://developer.x-plane.com/sdk/XPLMDataAccess/
-//https://developer.x-plane.com/sdk/XPLMWeather/
 
-//TODO: Research dataref paths
-//https://developer.x-plane.com/datarefs/
+
+FILE * logFile;
+static XPLMDataRef latDataRef = NULL;
+static XPLMDataRef lonDataRef = NULL;
+
+float PollData()
+{
+    fprintf(logFile, "Polling Data \n");
+    //Poll for data refs
+    float lat = XPLMGetDataf(latDataRef);
+    float lon = XPLMGetDataf(lonDataRef);
+
+    fprintf(logFile, "lat: %f, lon: %f\n", lat, lon);
+    //gather data
+
+    //compile packet
+
+    //send over network
+    return 1.0;
+}
 
 PLUGIN_API int XPluginStart(char * name, char * sig, char * desc)
 {
+    char outFilePath[512];
+
     //basic plugin information
     strcpy(name, "BeigeBox Recorder");
     strcpy(sig, "com.avidata.recorder");
     strcpy(desc, "Sim Flight Data Recorder for varied data");
 
-    //start network connection
+
+    //create path for log
+    XPLMGetSystemPath(outFilePath);
+    strcat(outFilePath, "TestValues.txt");
+    logFile = fopen(outFilePath, "w");
+
+    try
+    {
+
+    }
+    catch (...)
+    {
+
+    }
+    //init data ref
+    lonDataRef = XPLMFindDataRef("sim/flightmodel/position/longititude");
+    latDataRef = XPLMFindDataRef("sim/flightmodel/position/latitude");
+
+
 
     //register callback
+    XPLMRegisterFlightLoopCallback((XPLMFlightLoop_f)PollData, 1.0, NULL);
     return 1;
 }
 
@@ -50,6 +93,8 @@ PLUGIN_API void XPluginStop(void)
 {
     //end network connection
     //destroy callback
+    XPLMUnregisterFlightLoopCallback((XPLMFlightLoop_f)PollData, NULL);
+    fclose(logFile);
 }
 
 PLUGIN_API int XPluginEnable(void)
@@ -59,6 +104,7 @@ PLUGIN_API int XPluginEnable(void)
 
 PLUGIN_API void XPluginDisable(void)
 {
+    fflush(logFile);
 }
 
 PLUGIN_API void XPluginReceiveMessage(void)
@@ -66,13 +112,11 @@ PLUGIN_API void XPluginReceiveMessage(void)
 
 }
 
-void PollData()
-{
-    //Poll for data refs
+#ifdef _WIN32
+#include <windows.h>
 
-    //gather data
-
-    //compile packet
-
-    //send over network
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved) {
+    return TRUE;
 }
+
+#endif
